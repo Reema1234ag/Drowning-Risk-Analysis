29c3110200b833906c45ca51c9274891fb141202
Ritika Das <48116271+ritikadas18@users.noreply.github.com>
2020-11-07 23:06:56 +0530

    Created using Colaboratory

---
+++ VideoDataset/install_UCF11.py
@@ -0,0 +1,80 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "DrowningRiskAnalysis.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "toc_visible": true,
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "accelerator": "GPU"
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Reema1234ag/Drowning-Risk-Analysis/blob/master/VideoDataset/install_UCF11.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "HPFApGfHDyOL",
+        "outputId": "a803c3a9-28ec-4a6d-8a85-d3fa4132b164",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        }
+      },
+      "source": [
+        "!git clone https://github.com/Reema1234ag/Drowning-Risk-Analysis.git"
+      ],
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Cloning into 'Drowning-Risk-Analysis'...\n",
+            "remote: Enumerating objects: 15, done.\u001b[K\n",
+            "remote: Counting objects: 100% (15/15), done.\u001b[K\n",
+            "remote: Compressing objects: 100% (13/13), done.\u001b[K\n",
+            "remote: Total 15 (delta 4), reused 0 (delta 0), pack-reused 0\u001b[K\n",
+            "Unpacking objects: 100% (15/15), done.\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "GhiTKvnPW-aH"
+      },
+      "source": [
+        "import tensorflow as tf"
+      ],
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "Ob0KHBdCuacu"
+      },
+      "source": [
+        ""
+      ],
+      "execution_count": 1,
+      "outputs": []
+    }
+  ]
+}
